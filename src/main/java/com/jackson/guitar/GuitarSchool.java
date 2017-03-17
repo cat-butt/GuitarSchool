b3a231132cecbdd648f99b6327f30c294783e13b
Tobin Jackson <tobin.jackson@siemens.com>
2017-03-17 16:37:08 -0500

    Priliminary menu work

--- src/main/java/com/jackson/guitar/GuitarSchool.java
+++ src/main/java/com/jackson/guitar/GuitarSchool.java
@@ -8,6 +8,8 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -23,14 +25,21 @@ public class GuitarSchool extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        MenuBar menuBar = new MenuBar();
+        Menu menuFile = new Menu("File");
+        Menu menuEdit = new Menu("Edit");
+        Menu menuView = new Menu("View");
+        Menu menuAbout = new Menu("About");
+        menuBar.getMenus().addAll(menuFile, menuEdit, menuView, menuAbout);
+
         GuitarCanvas guitarCanvase = new GuitarCanvas();
 
         PianoCanvas pianoCanvas = new PianoCanvas();
 
         Group root = new Group();
         VBox vBox = new VBox();
-        vBox.getChildren().addAll(guitarCanvase, pianoCanvas);
-        root.getChildren().add(vBox);
+        vBox.getChildren().addAll(menuBar, guitarCanvase, pianoCanvas);
+        root.getChildren().addAll(vBox);
 
         Scene scene = new Scene(root);
 
